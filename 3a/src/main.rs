4154ce07b049fa75282309732e8b5102c7043ca1
Harmen <harmen.sielcken@gmail.com>
2023-12-19 01:33:02 +0100

    Fix formatting

--- 3a/src/main.rs
+++ 3a/src/main.rs
@@ -5,41 +5,50 @@ use std::clone::Clone;
 #[derive(Debug, Clone)]
 struct PotentialPart {
     value: String,
-    length: i32,
-    x0: i32,
-    y0: i32,
+    length: usize,
+    x0: Option<usize>,
+    y0: Option<usize>,
 }
 
 fn main() {
-    let line = "....4534....234...";
+    let test_input = r#"467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."#;
 
-    let potential_parts = extract_potential_parts_from_line(line);
+    let potential_parts: Vec<PotentialPart> = test_input
+        .lines()
+        .enumerate()
+        .flat_map(|(y0, line)| extract_potential_parts_from_line(line, y0))
+        .collect();
 
     println!("{:?}", potential_parts);
 }
 
-fn extract_potential_parts_from_line(line: &str) -> Vec<PotentialPart> {
-    let part = init_potential_part(0);
-
-    let init_potential_parts: Vec<PotentialPart> = vec![];
+fn extract_potential_parts_from_line(line: &str, y0: usize) -> Vec<PotentialPart> {
+    let init_potential_parts: Vec<PotentialPart> = vec![init_potential_part(None, Some(y0))];
 
     let potential_parts =
         line.chars()
             .enumerate()
-            .fold(init_potential_parts, |mut result, (i, char)| {
-                match char.to_string().parse::<i32>() {
+            .fold(init_potential_parts, |mut result, (x0, char)| {
+                match char.to_string().parse::<usize>() {
                     Ok(_) => {
-                        let mut init = part.clone();
+                        let potential_part = result
+                            .last_mut()
+                            .unwrap_or_else(|| panic!("Something went wrong"));
 
-                        let potential_part = result.last_mut().unwrap_or(&mut init);
-
-                        potential_part.length += 1;
-                        potential_part.value.push(char);
+                        update_potential_part(x0, char, potential_part);
                         return result;
                     }
                     Err(_) => {
-                        let new_potential_part = init_potential_part(i as i32);
-
+                        let new_potential_part = init_potential_part(None, Some(y0));
                         result.push(new_potential_part);
                         return result;
                     }
@@ -54,11 +63,22 @@ fn extract_potential_parts_from_line(line: &str) -> Vec<PotentialPart> {
     return filtered_potential_parts;
 }
 
-fn init_potential_part(x0: i32) -> PotentialPart {
+
+
+fn update_potential_part(x0: usize, char: char, potential_part: &mut PotentialPart) -> () {
+    if potential_part.x0.is_none() {
+        potential_part.x0 = Some(x0)
+    }
+
+    potential_part.length += 1;
+    potential_part.value.push(char);
+}
+
+fn init_potential_part(x0: Option<usize>, y0: Option<usize>) -> PotentialPart {
     return PotentialPart {
         value: "".to_string(),
         length: 0,
         x0,
-        y0: 0,
+        y0,
     };
 }
