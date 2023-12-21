d16fd86b011a982d25fbd7a7d4c786d568926f6b
Harmen <harmen.sielcken@gmail.com>
2023-12-21 01:39:45 +0100

    Last refactor

--- 4b/src/main.rs
+++ 4b/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
   let path = "4.dat";
   let file_contents = read::read_string_from_file(path);
 
-  let start_cards_count = get_line_count(&file_contents);
+  let starting_cards_count = get_line_count(&file_contents);
 
   let cards = file_contents.lines().map(|line| line_to_card(line));
 
@@ -15,16 +15,15 @@ fn main() {
     .enumerate()
     .map(|(card_index, numbers)| ((card_index + 1) as i32, get_matching_numbers_count(numbers)));
 
-  let initial_map = generate_initial_map(start_cards_count);
-
   let total_number_of_scratch_cards: i32 = matching_numbers_per_card
-    .fold(initial_map, |map, (card_number, score)| {
-      let multiplier = *map.get(&card_number).unwrap();
-      let maximum_card_index = start_cards_count.min(score + card_number);
+    .fold(HashMap::new(), |mut map, (card_index, score)| {
+      let current_card_count_at_root = *map.entry(card_index).or_insert(1);
 
-      return ((card_number + 1)..(maximum_card_index + 1)).fold(map, |mut result, x| {
-        let current_card_count = result.get(&x).unwrap();
-        let new_card_count = current_card_count + multiplier * 1;
+      let maximum_card_index = starting_cards_count.min(score + card_index);
+
+      return ((card_index + 1)..(maximum_card_index + 1)).fold(map, |mut result, x| {
+        let current_card_count = *result.entry(x).or_insert(1);
+        let new_card_count = current_card_count + current_card_count_at_root;
         result.insert(x, new_card_count);
         return result;
       });
@@ -74,14 +73,6 @@ fn string_to_set(string: &str) -> Vec<i32> {
 
 fn remove_prefix(line: &str) -> &str {
   return line.split(":").nth(1).unwrap();
-}
-
-fn generate_initial_map(number_of_cards: i32) -> HashMap<i32, i32> {
-  let init_map: HashMap<i32, i32> = HashMap::new();
-  return (1..(number_of_cards + 1)).fold(init_map, |mut map, x| {
-    map.insert(x as i32, 1);
-    return map;
-  });
 }
 
 fn get_line_count(file_contents: &str) -> i32 {
